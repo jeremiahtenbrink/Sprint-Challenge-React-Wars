b47de05e6b0067d8b1132e890f72dc5005f7ebc9
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-02-15 17:08:07 -0700

    Fixed bug

--- starwars/src/components/GetData.js
+++ starwars/src/components/GetData.js
@@ -2,14 +2,16 @@ export function GetData( url, cb ) {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    let data = localStorage.getItem( url );
+    let data = localStorage.getItem(url);
     if( data === undefined  || data === "undefined" || data == null) {
+        debugger;
         if( url === undefined || url === null ) {
             cb( [] );
             return;
         }
         fetch( url )
             .then( res => {
+                debugger;
                 return res.json();
             } )
             .then( data => {
@@ -20,10 +22,10 @@ export function GetData( url, cb ) {
             .catch( err => {
                 cb( [] );
             } );
+    }else {
+        debugger;
+        data = JSON.parse( data );
+        cb( data );
     }
-    
-    debugger;
-    data = JSON.parse( data );
-    cb( data );
     
 }
